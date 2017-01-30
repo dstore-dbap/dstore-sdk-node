9cf067124d692689065c3f360e5b3676587155d1
Björn Dolkemeier <b.dolkemeier@dbap.de>
2017-01-30 12:08:37 +0100

    Fixed values helper after 1.4.0 update

--- dstore/helper/valuesHelper.js
+++ dstore/helper/valuesHelper.js
@@ -1,13 +1,13 @@
 var Decimal = require('decimal.js');
 
 exports.toDate = function (timestampValue) {
-    if (!timestampValue instanceof proto.dstore.values.timestampValue)
-        throw "expected a timestampValue";
+    if (!timestampValue instanceof proto.dstore.values.TimestampValue)
+        throw "expected a TimestampValue";
     return /** @type {Date}  */ new Date(timestampValue.getValue().getSeconds() * 1000 + timestampValue.getValue().getNanos() / 1000000);
 };
 
 exports.toDecimal = function (decimalValue) {
-    if (!decimalValue instanceof proto.dstore.values.decimalValue)
-        throw "expected a timestampValue";
+    if (!decimalValue instanceof proto.dstore.values.DecimalValue)
+        throw "expected a DecimalValue";
     return /** @type {Decimal}  */ new Decimal(decimalValue.getValue());
 };
